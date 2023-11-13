a2b1fd73a3f96d5878bc02e01476e5e032e176fd
Akash Sharma <aakashsharma19090@gmail.com>
2023-11-13 13:23:30 +0530

    remove test console

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image';
 
 import Link from 'next/link';
 export default function Page() {
-    console.log('test commit');
     return (
         <main className="flex min-h-screen flex-col p-6">
             <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
